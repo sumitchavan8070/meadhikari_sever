1d6f9b04009a5c399f71137d742ff1f67cc5a8ad
Sumit Chavan <sdchavan8070@gmail.com>
2024-12-22 11:59:25 +0530

    test

--- server.js
+++ server.js
@@ -58,20 +58,20 @@ app.use("/api/v1/auth/coupons", require("./routes/couponRoutes"));
 // app.use("/api/v1/admin/coupons", require("./routes/couponRoutes"));
 
 // app.use("/api/v1/admin", require("./routes/adminRoutes"));
-// app.use("/api/v1/auth/variable", require("./routes/constantsRoutes"));
+app.use("/api/v1/auth/variable", require("./routes/constantsRoutes"));
 
-// app.use("/api/v1/auth/policy", require("./routes/privacyPolicyRoute"));
-// app.use("/api/v1/auth/deleteaccount", require("./routes/deletionRoutes"));
+app.use("/api/v1/auth/policy", require("./routes/privacyPolicyRoute"));
+app.use("/api/v1/auth/deleteaccount", require("./routes/deletionRoutes"));
 
 // app.use("/api/v1/admin", require("./routes/studentsTableBackedRoute"));
 // app.use("/api/v1/admin/donation", require("./routes/donationRoutes"));
-// app.use("/api/v1/auth/donation", require("./routes/donationRoutes"));
+app.use("/api/v1/auth/donation", require("./routes/donationRoutes"));
 
-// app.use("/api/v1/auth/posts", require("./routes/postRoutes"));
+app.use("/api/v1/auth/posts", require("./routes/postRoutes"));
 // app.use("/api/v1/admin/posts", require("./routes/postRoutes"));
 
-// app.use("/api/v1/auth/exam-categories", require("./routes/examCategoryRoutes")); //DONE
-// app.use("/api/v1/auth/subcategories", require("./routes/subExamTypeRoutes")); // Done
+app.use("/api/v1/auth/exam-categories", require("./routes/examCategoryRoutes")); //DONE
+app.use("/api/v1/auth/subcategories", require("./routes/subExamTypeRoutes")); // Done
 // app.use("/api/v1/admin/subcategories", require("./routes/subExamTypeRoutes")); // Done
 
 // app.use("/api/v1/auth/years", require("./routes/examYearRoutes")); // Done , for frontend
