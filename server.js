11203317e00577e27e237437d9a8690085fd42bb
Sumit Chavan <sdchavan8070@gmail.com>
2025-01-18 12:04:29 +0530

    test cors

--- server.js
+++ server.js
@@ -32,11 +32,23 @@ connectDB();
 //Rest Object
 
 //middleware
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "public, max-age=3600");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, DELETE"
+  );
+
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json());
 app.use(morgan("dev"));
 
